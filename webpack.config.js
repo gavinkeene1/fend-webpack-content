1d1386c7c2be659c589de0e97ec3a34e1dfac17d
Gavin Keene <64376895+gavinkeene1@users.noreply.github.com>
2020-06-05 17:49:36 -0400

    Add Reference to HTML Webpack Plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 // Add necessary requirement statements and module.exports
 const path = require('path');
 const webpack = require('webpack');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     entry: './src/client/index.js',
@@ -12,5 +13,11 @@ module.exports = {
                 loader: "babel-loader"
             }
         ]
-    }
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html",
+        })
+    ]
 }
