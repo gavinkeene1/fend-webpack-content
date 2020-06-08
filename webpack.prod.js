1b51e86e1c989f50f3f65644bd71a39a71242c88
Gavin Keene <64376895+gavinkeene1@users.noreply.github.com>
2020-06-08 19:55:37 -0400

    Add Clean-Webpack-Plugin to Webpack Config Files

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,6 +2,7 @@
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     mode: 'production',
@@ -19,6 +20,15 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
+        }),
+        new CleanWebpackPlugin({
+            // Simulate the removal of files
+            dry: true,
+            // Write logs to the Console
+            verbose: true,
+            // Automatically remove all unused webpack assets on rebuild
+            cleanStaleWebpackAssets: true,
+            protectWebpackAssets: false
         })
     ]
 }
